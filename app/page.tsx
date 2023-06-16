2d7a6c1baff4f71b5777f7885dca112684d79963
Marko <marko.uksanovic1@gmail.com>
2023-06-16 22:20:15 +1000

    added login, register routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
-  return (
-    <section>
-      Home
-    </section>
-  )
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
+import User from "./components/user";
+
+export default async function Home() {
+  return <div>Hi</div>;
 }
