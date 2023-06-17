05fbaa3f1d45fea90911ab18dd99c44b0bc3e233
Marko <marko.uksanovic1@gmail.com>
2023-06-18 00:49:45 +1000

    Misc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,27 @@
+import Navbar from "@/components/Navbar";
+import { Toaster } from "@/components/ui/Toaster";
 import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
 import { Inter } from "next/font/google";
 
 export const metadata = {
-  title: "Breadit",
+  title: "Shredit",
   description: "A Reddit clone built with Next.js and TypeScript.",
 };
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children, authModal }: { children: React.ReactNode; authModal: React.ReactNode }) {
   return (
     <html lang="en" className={cn("bg-white text-slate-900 antialias light", inter.className)}>
       <body className="min-h-screen pt-12 bg-slate-50 antialiased">
-        {/* <Navbar /> */}
+        {/* @ts-expect-error server-component*/}
+        <Navbar />
+
+        {authModal}
+
         <div className="container max-w-7xl mx-auto h-full pt-12">{children}</div>
+        <Toaster />
       </body>
     </html>
   );
